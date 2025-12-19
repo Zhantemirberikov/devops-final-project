74034602e210a8847732964a4540eeaa8af3cccf
Zhantemir Berikov <frogfather957@gmail.com>
2025-12-19 07:17:26 +0000

    feat: final devops project completed

--- app/src/main/java/com/zhantemir/devopsapp/DevopsappApplication.java
+++ app/src/main/java/com/zhantemir/devopsapp/DevopsappApplication.java
@@ -2,6 +2,8 @@ package com.zhantemir.devopsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DevopsappApplication {
@@ -10,4 +12,17 @@ public class DevopsappApplication {
         SpringApplication.run(DevopsappApplication.class, args);
     }
 
+    @RestController
+    public static class HealthController {
+
+        @GetMapping("/")
+        public String home() {
+            return "Application is UP and running!";
+        }
+
+        @GetMapping("/hello")
+        public String hello() {
+            return "Hello from Kubernetes deployment!";
+        }
+    }
 }
